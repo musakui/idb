611077d87dc88392ec109ee3b85777f614ac6ee9
randName <kwokshungit@gmail.com>
2026-03-16 15:03:31 +0800

    meh

--- lib/db.ts
+++ lib/db.ts
@@ -1,58 +1,24 @@
-export type IndexOptions = {
+interface IndexParams extends IDBIndexParameters {
 	path: string | Iterable<string>
-	unique?: boolean
-	multiEntry?: boolean
 }
 
-type DeleteStore = {
-	type: 'store.delete'
-	name: string
+interface StoreParams extends IDBObjectStoreParameters {
+	indexes?: Record<string, IndexParams>
 }
 
-type CreateStore = {
-	type: 'store.create'
-	name: string
-	opts?: IDBObjectStoreParameters
-	indexes?: Record<string, IndexOptions>
-}
-
-type DeleteIndex = {
-	type: 'index.delete'
-	store: string
-	name: string
-}
-
-type CreateIndex = {
-	type: 'index.create'
-	store: string
-	name: string
-	opts: IndexOptions
-}
-
-export type IDBMigration = CreateStore | DeleteStore | CreateIndex | DeleteIndex
-
-export function applyMigrations(tx: IDBTransaction, migrations: IDBMigration[]) {
-	for (const m of migrations) {
-		switch (m.type) {
-			case 'store.create': {
-				const store = tx.db.createObjectStore(m.name, m.opts)
-				for (const [name, { path, ...opts }] of Object.entries(m.indexes ?? {})) {
-					store.createIndex(name, path, opts)
-				}
-			}
-			case 'store.delete': {
-				tx.db.deleteObjectStore(m.name)
-				break
-			}
-			case 'index.create': {
-				const { path, ...opts } = m.opts
-				tx.objectStore(m.store).createIndex(m.name, path, opts)
-				break
-			}
-			case 'index.delete': {
-				tx.objectStore(m.store).deleteIndex(m.name)
-				break
-			}
+export function createStores(db: IDBDatabase, stores: Record<string, StoreParams>) {
+	for (const [name, { indexes, ...dbopts }] of Object.entries(stores)) {
+		const store = db.createObjectStore(name, dbopts)
+		if (!indexes) continue
+		for (const [name, { path, ...opts }] of Object.entries(indexes)) {
+			store.createIndex(name, path, opts)
 		}
 	}
+}
+
+type ExtractStoreKey<T extends Record<string, unknown>, P extends StoreParams> = P extends { keyPath: infer K } ? T[K] : unknown
+
+type A = ExtractStoreKey<{ id: number, name: string }, { keyPath: 'name' }>
+
+function makeDb<Stores extends Record<string, StoreParams>>(name: string, opts: { version?: number; stores: Stores }) {
 }
